f7c33c8bd44f23cc7898f62f871bd15a699fd787
Marius Alexandrescu <icethegr8@gmail.com>
2024-03-28 08:27:47 +0200

    start to add logic to script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Henlo!")
+// console.log("Henlo!")
+
+function getComputerChoice(){
+    let choice =  Math.floor(Math.random() * 3);
+    console.log(choice);
+    if (choice === 0){
+        choice = 'Rock';
+    }
+    else if (choice === 1){
+        choice = 'Paper';
+    }
+    else {
+        choice = 'Scissors'
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
