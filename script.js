30b6ebb39f826b395a2a61d286057cbb08ef01f9
Marius Alexandrescu <icethegr8@gmail.com>
2024-03-29 07:11:26 +0200

    add more logic to script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // console.log("Henlo!")
 
-let choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     let choice = choices[(Math.floor(Math.random() * choices.length))];
     return choice
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function getUserInput() {
+    let choice = prompt("Choose rock, paper or scissors").toLowerCase();
+    do {
+        choice = prompt("Choose rock, paper or scissors").toLowerCase();
+    }
+    while (!choices.includes(choice));
+    return choice;
+}
 
 function playRound(playerSelection, computerSelection) {
-
+    if(computerSelection == "rock") {
+        return "draw";
+    }
+    else if(computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if(computerSelection == "scissors") {
+        return "You Win! Rock beats Scrissors"
+    }
 }
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log(computerSelection);
 console.log(playRound(playerSelection,computerSelection));
+console.log(getUserInput());
